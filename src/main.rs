993a27806de9a02e58358cf2273ddc38e06dd8fb
Sergey <s12bugaga@gmail.com>
2022-04-14 17:06:25 +0300

    add readme

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use main_logic::main_logic::main_logic_functions;
 fn main() {
     let mut example = String::new();
     std::io::stdin().read_line(&mut example);
-    //example = String::from("58*31+85*(10/10-3)/7*7");
+    //example = String::from("5()");
     let result = main_logic_functions::main_func(&example);
     println!("{} = {}", &example, result);
 }
